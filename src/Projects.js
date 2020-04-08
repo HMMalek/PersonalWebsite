0e3c39ccddce9f13c078fe3c2ada71dac0bfa63c
Hasna Malek <hasna.m.malek@gmail.com>
2020-04-08 21:00:47 +0200

    Update Projects.js

--- src/Projects.js
+++ src/Projects.js
@@ -9,7 +9,8 @@ export default class Projects extends Component {
 
         <div id="reverb">
           <h3>Measuring reverberations in a room using a logSweep</h3>
-          <p> Description</p>
+          <p> The goal of this project is being able to get an acoustic information about a room without any knowledge of its geometry.<br/>
+               To do so, you can emit a logsweep in the room and recorded. Then you can extract the RT60 from the energy's curve.</p>
           <a  className="App-link"  href="https://github.com/HMMalek/ReverberationMeasurement" target="_blank" rel="noopener noreferrer" > project </a>
         </div>
         <div id="weather">
@@ -19,7 +20,7 @@ export default class Projects extends Component {
         </div>
         <div id="paper">
           <h3>Pydiogment</h3>
-          <p> Description</p>
+          <p> A contributor in the Pydiogment project</p>
           <a  className="App-link"  href="https://github.com/SuperKogito/pydiogment" target="_blank" rel="noopener noreferrer" > project </a>
         </div>
 
