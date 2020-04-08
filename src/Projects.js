c2f4b72c43a3fa0a98178ba28ba95d98bd055ad1
HMMalek <hasna.m.malek@gmail.com>
2020-04-08 19:32:44 +0200

    first commit

--- src/Projects.js
+++ src/Projects.js
@@ -10,17 +10,17 @@ export default class Projects extends Component {
         <div id="reverb">
           <h3>Measuring reverberations in a room using a logSweep</h3>
           <p> Description</p>
-          <a  className="App-link"  href="https://reactjs.org" target="_blank" rel="noopener noreferrer" > project </a>
+          <a  className="App-link"  href="https://github.com/HMMalek/ReverberationMeasurement" target="_blank" rel="noopener noreferrer" > project </a>
         </div>
         <div id="weather">
           <h3>A weather application using ReactJs</h3>
           <p> Description</p>
-          <a  className="App-link"  href="https://reactjs.org" target="_blank" rel="noopener noreferrer" > project </a>
+          <a  className="App-link"  href="https://hmmalek.github.io/WeatherApp/" target="_blank" rel="noopener noreferrer" > project </a>
         </div>
         <div id="paper">
           <h3>Pydiogment</h3>
           <p> Description</p>
-          <a  className="App-link"  href="https://reactjs.org" target="_blank" rel="noopener noreferrer" > project </a>
+          <a  className="App-link"  href="https://github.com/SuperKogito/pydiogment" target="_blank" rel="noopener noreferrer" > project </a>
         </div>
 
     </div>
